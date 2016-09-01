f2842a8d9d070915496a64d8c72df6767075980f
Edward Carroll <ed@edcarroll.co.uk>
2016-09-01 02:00:42 +0100

    Further angular cli restructuring

--- demo/app/app.routing.ts
+++ demo/app/app.routing.ts
@@ -77,4 +77,4 @@ const appRoutes: Routes = [
 
 export const appRoutingProviders: any[] = [];
 
-export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes, { useHash: true });
