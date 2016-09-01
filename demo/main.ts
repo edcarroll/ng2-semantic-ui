40c56df7c0b3d93aa635048a5606924a91bbd83f
Edward Carroll <ed@edcarroll.co.uk>
2016-09-01 01:40:10 +0100

    Updated to latest angular cli

--- demo/main.ts
+++ demo/main.ts
@@ -1,3 +1,4 @@
+import "./polyfills.ts";
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule, environment } from './app/';
