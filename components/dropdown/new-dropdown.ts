c6f67b4109223d206a63e135e6f786ad42230e54
Ed Carroll <ed@edcarroll.co.uk>
2017-02-03 01:37:29 +0000

    Added proper nesting support to new dropdown

--- components/dropdown/new-dropdown.ts
+++ components/dropdown/new-dropdown.ts
@@ -5,18 +5,50 @@ export class NewDropdownService {
     public isOpen:boolean;
     public isOpenChange:EventEmitter<boolean>;
 
+    public children:NewDropdownService[];
+    public isNested:boolean;
+
     constructor() {
         this.isOpen = false;
         this.isOpenChange = new EventEmitter<boolean>();
+
+        this.children = [];
+        this.isNested = false;
     }
 
     public setOpenState(isOpen:boolean) {
-        this.isOpen = isOpen;
-        this.isOpenChange.emit(isOpen);
+        if (this.isOpen != isOpen) {
+            this.isOpen = isOpen;
+            this.isOpenChange.emit(isOpen);
+
+            if (!isOpen) {
+                this.children.forEach(c => c.setOpenState(isOpen));
+            }
+        }
     }
 
     public toggleOpenState() {
         this.setOpenState(!this.isOpen);
+    }
+
+    public registerChild(child:NewDropdownService) {
+        if (!this.isChildRegistered(child)) {
+            this.children.push(child);
+            child.isNested = true;
+        }
+    }
+
+    public isChildRegistered(child:NewDropdownService):boolean {
+        return this.children
+            .map(c => (c == child || child.children
+                .map(c => c.isChildRegistered(child))
+                .reduce((a, b) => a || b, false)))
+            .reduce((a, b) => a || b, false);
+    }
+
+    public clearChildren() {
+        this.children.forEach(c => c.isNested = false);
+        this.children = [];
     }
 }
 
@@ -48,6 +80,7 @@ export class NewSuiDropdownMenu extends SuiTransition {
         e.stopPropagation();
 
         // We have selected a dropdown item
+        console.log("clicked item");
     }
 }
 
@@ -55,7 +88,7 @@ export class NewSuiDropdownMenu extends SuiTransition {
     selector: '[newSuiDropdown]'
 })
 export class NewSuiDropdown implements AfterContentInit {
-    private _service:NewDropdownService;
+    public service:NewDropdownService;
 
     @ContentChild(NewSuiDropdownMenu)
     private _menu:NewSuiDropdownMenu;
@@ -65,53 +98,53 @@ export class NewSuiDropdown implements AfterContentInit {
     private _children:QueryList<NewSuiDropdown>;
 
     public get children() {
-        if (!this._children) {
-            return [];
-        }
-
         return this._children.filter(c => c !== this);
     }
 
-    public isNested:boolean;
-
     @Output()
     public get isOpenChange() {
-        return this._service.isOpenChange;
+        return this.service.isOpenChange;
     }
 
     @HostBinding('class.active')
     public get isActive() {
-        return this._service.isOpen && !this.isNested;
+        return this.service.isOpen && !this.service.isNested;
     }
 
     @Input()
     public get isOpen() {
-        return this._service.isOpen;
+        return this.service.isOpen;
     }
 
     public set isOpen(value:boolean) {
-        this._service.setOpenState(value);
+        this.service.setOpenState(value);
     }
 
     constructor() {
-        this._service = new NewDropdownService();
-
-        this.isNested = false;
+        this.service = new NewDropdownService();
     }
 
     public ngAfterContentInit() {
         if (!this._menu) {
             throw new Error("You must set [suiDropdownMenu] on the menu element.");
         }
-        this._menu.service = this._service;
+        this._menu.service = this.service;
 
-        this.children.forEach(c => c.isNested = true);
+        this.childrenUpdated();
+        this._children.changes
+            .subscribe(() => this.childrenUpdated());
+    }
+
+    private childrenUpdated() {
+        this.children
+            .map(c => c.service)
+            .forEach(s => this.service.registerChild(s))
     }
 
     @HostListener("click", ['$event'])
     public onClick(e:MouseEvent) {
         e.stopPropagation();
 
-        this._service.toggleOpenState();
+        this.service.toggleOpenState();
     }
 }
