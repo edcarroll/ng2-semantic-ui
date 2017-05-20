edcarroll/ng2-semantic-ui

commit 846e592c29fa95a52ba15521fd09ca6e240b37ba

Fix removal of selected options if available options has zero length.

components/select/multi-select.ts
@@ -127,6 +127,9 @@ export class SuiMultiSelect<T, U> extends SuiSelectBase<T, U> implements AfterVi
                     this._writtenOptions = values;
                 }
             }
+            if (values.length === 0) {
+                this.selectedOptions = null;
+            }
         }
     }
 

commit b44da45730d698e20e9ac820572e2e23634f0cc6

Fix Cannot read property 'length' of null error
Fix an error that has been introduced in commit 846e592c, that clears
the selectedOptions to clear all labels from the mutliselect, when
options and selectedOptions were cleared. selectedOptions should be an
empty array instead of null.

components/select/multi-select.ts
@@ -128,7 +128,7 @@ export class SuiMultiSelect<T, U> extends SuiSelectBase<T, U> implements AfterVi
                 }
             }
             if (values.length === 0) {
-                this.selectedOptions = null;
+                this.selectedOptions = [];
             }
         }
     }