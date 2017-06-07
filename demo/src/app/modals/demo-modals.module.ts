dc97baacec726f362367bc654d8d7f4ce49ef932
Edward Carroll <ed@edcarroll.co.uk>
2017-06-07 01:50:32 +0100

    Moved demo to use modals rather than native alert

---
+++ demo/src/app/modals/demo-modals.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SuiModalModule} from '../../../../components/modal/modal.module';
+import {AlertModalComponent} from './alert.modal';
+import {ConfirmModalComponent} from './confirm.modal';
+
+@NgModule({
+    imports: [
+        CommonModule,
+
+        SuiModalModule
+    ],
+    declarations: [
+        AlertModalComponent,
+        ConfirmModalComponent
+    ],
+    exports: [
+        AlertModalComponent,
+        ConfirmModalComponent
+    ],
+    entryComponents: [
+        AlertModalComponent,
+        ConfirmModalComponent
+    ]
+})
+export class DemoModalsModule {}
