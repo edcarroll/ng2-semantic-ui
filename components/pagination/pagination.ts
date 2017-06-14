dab3a4b07ced58d603aead4a82d056e5a708db57
Massimo Costa <massimoc@filewave.com>
2017-06-14 21:34:45 +0200

    Fix tslint

--- components/pagination/pagination.ts
+++ components/pagination/pagination.ts
@@ -1,159 +1,163 @@
 import { Component, Input, Output, OnChanges, SimpleChanges, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: "sui-pagination",
-  template: `
+    selector: "sui-pagination",
+    template: `
 <div class="ui pagination menu">
-  <a *ngIf="showBoundary" class="item"  (click)="selectPage(1)" 
-  [ngClass]="{'disabled': page===1}"><i class="fast backward icon"></i></a>
-  <a *ngIf="showNavigation" class="item" (click)="selectPage(page-1)" 
-  [ngClass]="{'disabled': !hasPrevious()}"><i class="step backward icon"></i></a>
-  <a *ngFor="let p of pages" class="item" [ngClass]="{'active':p==page}" (click)="page=p">
-  {{ p }}
-  </a>
-  <a *ngIf="showNavigation" class="item" (click)="selectPage(page+1)" 
-  [ngClass]="{'disabled': !hasNext()}"><i class="step forward icon"></i></a>
-  <a *ngIf="showBoundary" class="item"  (click)="selectPage(pageCount)" 
-  [ngClass]="{'disabled': page===pageCount}"><i class="fast forward icon"></i></a>
+    <a *ngIf="showBoundary" class="item"  (click)="selectPage(1)" [ngClass]="{'disabled': page===1}">
+        <i class="fast backward icon"></i>
+    </a>
+    <a *ngIf="showNavigation" class="item" (click)="selectPage(page-1)" [ngClass]="{'disabled': !hasPrevious()}">
+        <i class="step backward icon"></i>
+    </a>
+    <a *ngFor="let p of pages" class="item" [ngClass]="{'active':p==page}" (click)="page=p">
+        {{ p }}
+    </a>
+    <a *ngIf="showNavigation" class="item" (click)="selectPage(page+1)" [ngClass]="{'disabled': !hasNext()}">
+        <i class="step forward icon"></i>
+    </a>
+    <a *ngIf="showBoundary" class="item"  (click)="selectPage(pageCount)" [ngClass]="{'disabled': page===pageCount}">
+        <i class="fast forward icon"></i>
+    </a>
 </div>
 `
 })
 export class SuiPagination implements OnChanges {
 
-  // Public members
-  public pages:number[] = [];
-  public pageCount:number;
+    // Public members
+    public pages:number[] = [];
+    public pageCount:number;
 
-  @Output()
-  public pageChanged:EventEmitter<number> = new EventEmitter<number>();
+    @Output()
+    public pageChanged:EventEmitter<number> = new EventEmitter<number>();
 
-  // Private members
-  private _maxSize:number;
-  private _collectionSize:number;
-  private _pageSize:number;
-  private _page:number;
-  private _showBoundary:boolean;
-  private _showNavigation:boolean;
+    // Private members
+    private _maxSize:number;
+    private _collectionSize:number;
+    private _pageSize:number;
+    private _page:number;
+    private _showBoundary:boolean;
+    private _showNavigation:boolean;
 
-  constructor() {
-    this.maxSize = 0;
-    this.collectionSize = 100;
-    this.pageSize = 10;
-    this.page = 0;
-    this.showNavigation = false;
-  }
-
-  // Public methods
-  @Input()
-  public get maxSize():number {
-    return this._maxSize;
-  }
-
-  public set maxSize(value:number) {
-    this._maxSize = value;
-  }
-
-  @Input()
-  public get collectionSize():number {
-    return this._collectionSize;
-  }
-
-  public set collectionSize(value:number) {
-    this._collectionSize = value;
-  }
-
-  @Input()
-  public get pageSize():number {
-    return this._pageSize;
-  }
-
-  public set pageSize(value:number) {
-    this._pageSize = value;
-  }
-
-  @Input()
-  public get showNavigation():boolean {
-    return this._showNavigation;
-  }
-
-  public set showNavigation(value:boolean) {
-    this._showNavigation = value;
-  }
-
-  @Input()
-  public get showBoundary():boolean {
-    return this._showBoundary;
-  }
-
-  public set showBoundary(value:boolean) {
-    this._showBoundary = value;
-  }
-
-  public get page():number {
-    return this._page;
-  }
-
-  public set page(value:number) {
-    if (value !== this._page) {
-    this._page = value;
-    this.pageChanged.emit(this._page);
-    }
-  }
-
-  public hasPrevious():boolean {
-    return this.page > 1;
-  }
-
-  public hasNext():boolean {
-    return this.page < this.pageCount;
-  }
-
-  public selectPage(newPage:number):void {
-    this.updatePages(newPage);
-  }
-
-  public ngOnChanges(chages:SimpleChanges):void {
-    this.updatePages(this.page);
-  }
-
-  // Private methods
-  private updatePages(newPage:number):void {
-    this.pageCount = Math.ceil(this._collectionSize / this._pageSize);
-
-    this.pages.length = 0;
-    for (let i = 0; i < this.pageCount; ++i) {
-    this.pages.push(i + 1);
+    constructor() {
+        this.maxSize = 0;
+        this.collectionSize = 100;
+        this.pageSize = 10;
+        this.page = 0;
+        this.showNavigation = false;
     }
 
-    this.page = this.setPageInRange(newPage);
-
-    if (this._maxSize > 0 && this._maxSize < this.pageCount) {
-    let start = 0;
-    let end = this.pageCount;
-
-    [start, end] = this.applyPagination();
-
-    this.pages = this.pages.slice(start, end);
-
-    this.showNavigation = true;
+    // Public methods
+    @Input()
+    public get maxSize():number {
+        return this._maxSize;
     }
-  }
 
-  private setPageInRange(newPage:number):number {
-    if (newPage < 1) {
-      return 1;
-    } else if (newPage > this.pageCount) {
-      return this.pageCount;
+    public set maxSize(value:number) {
+        this._maxSize = value;
     }
-    return newPage;
-  }
 
-  private applyPagination():number[] {
-    const page = Math.ceil(this.page / this.maxSize) - 1;
-    const start = page * this.maxSize;
-    const end = start + this.maxSize;
+    @Input()
+    public get collectionSize():number {
+        return this._collectionSize;
+    }
 
-    return [start, end];
-  }
+    public set collectionSize(value:number) {
+        this._collectionSize = value;
+    }
+
+    @Input()
+    public get pageSize():number {
+        return this._pageSize;
+    }
+
+    public set pageSize(value:number) {
+        this._pageSize = value;
+    }
+
+    @Input()
+    public get showNavigation():boolean {
+        return this._showNavigation;
+    }
+
+    public set showNavigation(value:boolean) {
+        this._showNavigation = value;
+    }
+
+    @Input()
+    public get showBoundary():boolean {
+        return this._showBoundary;
+    }
+
+    public set showBoundary(value:boolean) {
+        this._showBoundary = value;
+    }
+
+    public get page():number {
+        return this._page;
+    }
+
+    public set page(value:number) {
+        if (value !== this._page) {
+            this._page = value;
+            this.pageChanged.emit(this._page);
+        }
+    }
+
+    public hasPrevious():boolean {
+        return this.page > 1;
+    }
+
+    public hasNext():boolean {
+        return this.page < this.pageCount;
+    }
+
+    public selectPage(newPage:number):void {
+        this.updatePages(newPage);
+    }
+
+    public ngOnChanges(chages:SimpleChanges):void {
+        this.updatePages(this.page);
+    }
+
+    // Private methods
+    private updatePages(newPage:number):void {
+        this.pageCount = Math.ceil(this._collectionSize / this._pageSize);
+
+        this.pages.length = 0;
+        for (let i = 0; i < this.pageCount; ++i) {
+            this.pages.push(i + 1);
+        }
+
+        this.page = this.setPageInRange(newPage);
+
+        if (this._maxSize > 0 && this._maxSize < this.pageCount) {
+            let start = 0;
+            let end = this.pageCount;
+
+            [start, end] = this.applyPagination();
+
+            this.pages = this.pages.slice(start, end);
+
+            this.showNavigation = true;
+        }
+    }
+
+    private setPageInRange(newPage:number):number {
+        if (newPage < 1) {
+            return 1;
+        } else if (newPage > this.pageCount) {
+            return this.pageCount;
+        }
+        return newPage;
+    }
+
+    private applyPagination():number[] {
+        const page = Math.ceil(this.page / this.maxSize) - 1;
+        const start = page * this.maxSize;
+        const end = start + this.maxSize;
+
+        return [start, end];
+    }
 
 }
--- demo/src/app/pages/pagination/pagination.page.ts
+++ demo/src/app/pages/pagination/pagination.page.ts
@@ -3,132 +3,134 @@ import { ApiDefinition } from "../../components/api/api.component";
 
 const exampleStandardTemplate = `
 <div class="ui segments">
-  <div class="ui segment">
-    <sui-pagination [collectionSize]="100" [pageSize]="10" [showNavigation]="showNavigation" [showBoundary]="showBoundary"
-      (pageChanged)="onPageChanged($event)"></sui-pagination>
-    <p>Current page: {{ selectedPage }}</p>
-  </div>
-  <div class="ui segment">
-    <button class="ui primary button" (click)="showNavigation = !showNavigation">
-      Toggle Navigation
-    </button>
-    <button class="ui primary button" (click)="showBoundary = !showBoundary">
-      Toggle Boundary
-    </button>
-  </div>
+    <div class="ui segment">
+        <sui-pagination [collectionSize]="100" [pageSize]="10" [showNavigation]="showNavigation" [showBoundary]="showBoundary"
+            (pageChanged)="onPageChanged($event)">
+        </sui-pagination>
+        <p>Current page: {{ selectedPage }}</p>
+    </div>
+    <div class="ui segment">
+        <button class="ui primary button" (click)="showNavigation = !showNavigation">
+            Toggle Navigation
+        </button>
+        <button class="ui primary button" (click)="showBoundary = !showBoundary">
+            Toggle Boundary
+        </button>
+    </div>
 </div>
 `;
 
 const exampleMaxSizeTemplate = `
 <div class="ui segments">
-  <div class="ui segment">
-    <sui-pagination [collectionSize]="100" [pageSize]="10" [maxSize]="5" [showBoundary]="showBoundary"
-      (pageChanged)="onPageChanged($event)"></sui-pagination>
-    <p>Current page: {{ selectedPage }}</p>
-  </div>
-  <div class="ui segment">
-    <button class="ui primary button" (click)="showBoundary = !showBoundary">
-      Toggle Boundary
-    </button>
-  </div>
+    <div class="ui segment">
+        <sui-pagination [collectionSize]="100" [pageSize]="10" [maxSize]="5" [showBoundary]="showBoundary"
+            (pageChanged)="onPageChanged($event)">
+        </sui-pagination>
+        <p>Current page: {{ selectedPage }}</p>
+    </div>
+    <div class="ui segment">
+        <button class="ui primary button" (click)="showBoundary = !showBoundary">
+            Toggle Boundary
+        </button>
+    </div>
 </div>
 `;
 
 @Component({
-  selector: "demo-page-pagination",
-  templateUrl: "./pagination.page.html"
+    selector: "demo-page-pagination",
+    templateUrl: "./pagination.page.html"
 })
 export class PaginationPage implements OnInit {
-  public api:ApiDefinition = [
-    {
-      selector: "<sui-pagination>",
-      properties: [
+    public api:ApiDefinition = [
         {
-          name: "collectionSize",
-          type: "number",
-          description: "The number of items in the collection",
-          defaultValue: "100",
-          required: true
-        },
-        {
-          name: "maxSize",
-          type: "number",
-          description: "The maximum number of items shown",
-          defaultValue: "0"
-        },
-        {
-          name: "pageSize",
-          type: "number",
-          description: "The number of items in each page",
-          defaultValue: "10",
-          required: true
-        },
-        {
-          name: "showNavigation",
-          type: "boolean",
-          description: "Show/Hide te prev/next items. Automatically set to true is maxSize < pageCount",
-          defaultValue: "false"
+            selector: "<sui-pagination>",
+            properties: [
+                {
+                    name: "collectionSize",
+                    type: "number",
+                    description: "The number of items in the collection",
+                    defaultValue: "100",
+                    required: true
+                },
+                {
+                    name: "maxSize",
+                    type: "number",
+                    description: "The maximum number of items shown",
+                    defaultValue: "0"
+                },
+                {
+                    name: "pageSize",
+                    type: "number",
+                    description: "The number of items in each page",
+                    defaultValue: "10",
+                    required: true
+                },
+                {
+                    name: "showNavigation",
+                    type: "boolean",
+                    description: "Show/Hide te prev/next items. Automatically set to true is maxSize < pageCount",
+                    defaultValue: "false"
+                }
+            ],
+            events: [
+                {
+                    name: "pageChanged",
+                    type: "number",
+                    description: "Event fired when the current page changes"
+                }
+            ]
         }
-      ],
-      events: [
-        {
-          name: "pageChanged",
-          type: "number",
-          description: "Event fired when the current page changes"
-        }
-      ]
-    }
-  ];
+    ];
 
-  public exampleStandardTemplate:string = exampleStandardTemplate;
-  public exampleMaxSizeTemplate:string = exampleMaxSizeTemplate;
+    public exampleStandardTemplate:string = exampleStandardTemplate;
+    public exampleMaxSizeTemplate:string = exampleMaxSizeTemplate;
 
-  constructor() { }
+    constructor() { }
 
-  public ngOnInit():void { }
+    public ngOnInit():void { }
 }
 
 
 @Component({
-  selector: "example-pagination-standard",
-  template: exampleStandardTemplate
+    selector: "example-pagination-standard",
+    template: exampleStandardTemplate
 })
 export class PaginationExampleStandard implements OnInit {
 
-  public selectedPage:number;
-  public showNavigation:boolean;
-  public showBoundary:boolean;
+    public selectedPage:number;
+    public showNavigation:boolean;
+    public showBoundary:boolean;
 
-  constructor() { }
+    constructor() { }
 
-  public ngOnInit():void {
-    this.showNavigation = false;
-    this.showBoundary = false;
-  }
+    public ngOnInit():void {
+        this.showNavigation = false;
+        this.showBoundary = false;
+    }
 
-  public onPageChanged($event:number):void {
-    this.selectedPage = $event;
-  }
+    public onPageChanged($event:number):void {
+        this.selectedPage = $event;
+    }
 }
 
 @Component({
-  selector: "example-pagination-maxsize",
-  template: exampleMaxSizeTemplate
+    selector: "example-pagination-maxsize",
+    template: exampleMaxSizeTemplate
 })
 export class PaginationExampleMaxSize implements OnInit {
 
-  public selectedPage:number;
-  public showBoundary:boolean;
+    public selectedPage:number;
+    public showBoundary:boolean;
 
-  constructor() { }
+    constructor() { }
 
-  public ngOnInit():void {
-    this.showBoundary = false;
-  }
+    public ngOnInit():void {
+        this.showBoundary = false;
+    }
 
-  public onPageChanged($event:number):void {
-    this.selectedPage = $event;
-  }
+    public onPageChanged($event:number):void {
+        this.selectedPage = $event;
+    }
 }
 
 export const PaginationPageComponents = [PaginationPage, PaginationExampleStandard, PaginationExampleMaxSize];
