c55a673a0ed8eaccc7a5b37a107c7d6dcfb38689
Ed Carroll <ed@edcarroll.co.uk>
2017-06-26 21:27:03 +0100

    feat(datepicker): Added initial mobile fallback code

--- src/datepicker/directives/input.directive.ts
+++ src/datepicker/directives/input.directive.ts
@@ -2,36 +2,68 @@
 import { Directive, Host, Input, ElementRef, HostBinding, HostListener } from "@angular/core";
 import { SuiDatepickerDirective } from "./datepicker.directive";
 import { IDateParser, DateParser } from "../classes/date-parser";
+import { PopupTrigger } from "../../popup/classes/popup-config";
 
 @Directive({
     selector: "input[suiDatepicker]"
 })
 export class SuiDatepickerInputDirective {
-    @Input()
-    public mobileFallback:boolean;
+    private _mobileFallback:boolean;
+
+    public get mobileFallback():boolean {
+        return this._mobileFallback;
+    }
+
+    public set mobileFallback(fallback:boolean) {
+        this._mobileFallback = fallback;
+
+    }
+
+    private _fallbackActive:boolean;
+
+    public get fallbackActive():boolean {
+        return this._fallbackActive;
+    }
+
+    public set fallbackActive(active:boolean) {
+        this._fallbackActive = active;
+        this.datepicker.popup.config.trigger = this.fallbackActive ? PopupTrigger.Manual : PopupTrigger.Focus;
+        this.updateValue(this.selectedDateString);
+    }
 
     private _currentInputValue:string | undefined;
 
     public get selectedDateString():string | undefined {
         if (this.datepicker.selectedDate) {
-            return this.datepicker.config.parser.format(this.datepicker.selectedDate);
+            const formatted = this.datepicker.config.parser.format(this.datepicker.selectedDate);
+            if (this.fallbackActive) {
+                return formatted.replace(" ", "T");
+            }
+            return formatted;
         }
     }
 
     @HostBinding("attr.type")
     public get HTMLType():string {
+        if (this.fallbackActive) {
+            return this.datepicker.config.fallback;
+        }
         return "text";
     }
 
     constructor(@Host() public datepicker:SuiDatepickerDirective, public element:ElementRef) {
         this.mobileFallback = true;
+        this.fallbackActive = true;
 
-        this.datepicker.onDateChange.subscribe(() => {
-            if (this.selectedDateString && this.selectedDateString !== this._currentInputValue) {
-                this.datepicker.renderer.setProperty(this.element.nativeElement, "value", this.selectedDateString);
-                this._currentInputValue = this.selectedDateString;
-            }
-        });
+        this.datepicker.onDateChange.subscribe(() =>
+            this.updateValue(this.selectedDateString));
+    }
+
+    private updateValue(value:string | undefined):void {
+        if (value && value !== this._currentInputValue) {
+            this._currentInputValue = value;
+            this.datepicker.renderer.setProperty(this.element.nativeElement, "value", this.selectedDateString);
+        }
     }
 
     @HostListener("input", ["$event.target.value"])
