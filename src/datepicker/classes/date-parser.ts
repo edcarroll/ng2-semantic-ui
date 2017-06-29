906114694b1daa6cc31cc985fa70b34a744b31b9
Ed Carroll <ed@edcarroll.co.uk>
2017-06-29 13:37:59 +0100

    feat(datepicker): Added full localisation support

--- src/datepicker/classes/date-parser.ts
+++ src/datepicker/classes/date-parser.ts
@@ -2,88 +2,29 @@ import { ILocaleValues } from "../../util/services/localization.service";
 import { Util } from "../../util/util";
 import { DatePrecision } from "../../util/helpers/date";
 import { DatepickerMode } from "../components/datepicker";
-import { format, parse } from "date-fns";
-import * as dateFnDefaultLocale from "date-fns/locale/en-US";
-import { IDatepickerFormatsLocaleValues, IDatepickerLocaleValues } from "../localization";
+import { DateFnsParser } from "../helpers/date-fns";
+import { IDatepickerLocaleValues, IDatepickerFormatsLocaleValues } from "../../../dist/datepicker/localization";
 
 export interface IDateParser {
     format(date:Date):string;
     parse(dateString:string, baseDate?:Date):Date;
 }
 
-interface IDateFnsLocaleValues { [name:string]:string[]; }
-type DateFnsLocaleIndexCallback = (i:number) => number;
-type DateFnsLocaleFunction = (dirtyIndex:number, dirtyOptions:{ type?:string }) => string;
-type DateFnsLocaleArrayFunction = (dirtyOptions:{ type?:string }) => string[];
-interface IDateFnLocale {
-    localize:{
-        ordinalNumber?:DateFnsLocaleFunction;
-        weekday:DateFnsLocaleFunction;
-        weekdays:DateFnsLocaleArrayFunction;
-        month:DateFnsLocaleFunction;
-        months:DateFnsLocaleArrayFunction;
-        timeOfDay?:DateFnsLocaleFunction;
-        timesOfDay?:DateFnsLocaleArrayFunction;
-    };
-    options?:{
-        weekStartsOn?:number;
-    };
-}
-
-function buildLocalizeFn(values:IDateFnsLocaleValues, defaultType:string, indexCallback?:DateFnsLocaleIndexCallback):DateFnsLocaleFunction {
-    return (dirtyIndex, dirtyOptions) => {
-        const options = dirtyOptions || {};
-        const type = options.type ? options.type : defaultType;
-        const valuesArray = values[type] || values[defaultType];
-        const index = indexCallback ? indexCallback(dirtyIndex) : dirtyIndex;
-        return valuesArray[index];
-    };
-}
-
-function buildLocalizeArrayFn(values:IDateFnsLocaleValues, defaultType:string):DateFnsLocaleArrayFunction {
-    return dirtyOptions => {
-        const options = dirtyOptions || {};
-        const type = options.type ? options.type : defaultType;
-        return values[type] || values[defaultType];
-    };
-}
-
-
 export class DateParser implements IDateParser {
     private _format:string;
-    private _locale:IDateFnLocale;
+    private _parser:DateFnsParser;
 
     constructor(format:string, locale:IDatepickerLocaleValues) {
         this._format = format;
-
-        const weekdayValues = {
-            long: locale.weekdays,
-            short: locale.weekdaysShort,
-            narrow: locale.weekdaysNarrow
-        };
-
-        const monthValues = {
-            long: locale.months,
-            short: locale.monthsShort
-        };
-
-        this._locale = dateFnDefaultLocale as any;
-        Object.assign(this._locale, {
-            localize: {
-                weekday: buildLocalizeFn(weekdayValues, "long"),
-                weekdays: buildLocalizeArrayFn(weekdayValues, "long"),
-                month: buildLocalizeFn(monthValues, "long"),
-                months: buildLocalizeArrayFn(monthValues, "long")
-            }
-        });
+        this._parser = new DateFnsParser(locale);
     }
 
     public format(date:Date):string {
-        return format(date, this._format, { locale: this._locale as any });
+        return this._parser.format(date, this._format);
     }
 
     public parse(dateString:string, baseDate:Date = new Date()):Date {
-        return parse(dateString, this._format, baseDate, { locale: this._locale as any });
+        return this._parser.parse(dateString, this._format, baseDate);
     }
 }
 
---
+++ src/datepicker/helpers/date-fns.ts
@@ -0,0 +1,101 @@
+import { IDatepickerLocaleValues } from "../../../dist/datepicker/localization";
+import { format, parse } from "date-fns";
+import * as defaultLocale from "date-fns/locale/en-US";
+
+interface IDateFnsLocaleValues { [name:string]:string[]; }
+interface IDateFnsHelperOptions { type?:string; }
+type DateFnsHelper<U, T> = (value:U, options:IDateFnsHelperOptions) => T;
+type DateFnsWeekStartsOn = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+
+interface IDateFnsCustomLocale {
+    localize:{
+        weekday:DateFnsHelper<number, string>;
+        weekdays:DateFnsHelper<IDateFnsHelperOptions, string[]>;
+        month:DateFnsHelper<number, string>;
+        months:DateFnsHelper<IDateFnsHelperOptions, string[]>;
+    };
+    match:{
+        weekdays:DateFnsHelper<string, RegExpMatchArray | null>;
+        weekday?:DateFnsHelper<RegExpMatchArray, number>;
+        months:DateFnsHelper<string, RegExpMatchArray | null>;
+        month?:DateFnsHelper<RegExpMatchArray, number>;
+    };
+    options?:{
+        weekStartsOn?:number;
+    };
+}
+
+function buildLocalizeFn(values:IDateFnsLocaleValues, defaultType:string):DateFnsHelper<number, string> {
+    return (dirtyIndex, { type } = { type: defaultType }) => values[type][dirtyIndex];
+}
+
+function buildLocalizeArrayFn(values:IDateFnsLocaleValues, defaultType:string):DateFnsHelper<IDateFnsHelperOptions, string[]> {
+    return ({ type } = { type: defaultType }) => values[type];
+}
+
+function buildMatchFn(patterns:IDateFnsLocaleValues, defaultType:string):DateFnsHelper<string, RegExpMatchArray | null> {
+    return (dirtyString, { type } = { type: defaultType }) =>
+        dirtyString.match(`^(${patterns[type].join("|")})`);
+}
+
+function buildParseFn(patterns:IDateFnsLocaleValues, defaultType:string):DateFnsHelper<RegExpMatchArray, number> {
+    return ([, result], { type } = { type: defaultType }) =>
+        (patterns[type] || patterns[defaultType])
+            .map(p => new RegExp(`^${p}`))
+            .findIndex(pattern => pattern.test(result));
+}
+
+export class DateFnsParser {
+    private _weekStartsOn:DateFnsWeekStartsOn;
+    private _locale:IDateFnsCustomLocale;
+
+    private get _config():any {
+        return {
+            weekStartsOn: this._weekStartsOn,
+            locale: this._locale
+        };
+    }
+
+    constructor(locale:IDatepickerLocaleValues) {
+        this._weekStartsOn = locale.firstDayOfWeek as DateFnsWeekStartsOn;
+
+        const weekdayValues = {
+            long: locale.weekdays,
+            short: locale.weekdaysShort,
+            narrow: locale.weekdaysNarrow
+        };
+
+        const monthValues = {
+            long: locale.months,
+            short: locale.monthsShort
+        };
+
+        this._locale = defaultLocale as any;
+        this._locale.localize = {
+            ...this._locale.localize,
+            ...{
+                weekday: buildLocalizeFn(weekdayValues, "long"),
+                weekdays: buildLocalizeArrayFn(weekdayValues, "long"),
+                month: buildLocalizeFn(monthValues, "long"),
+                months: buildLocalizeArrayFn(monthValues, "long")
+            }
+        };
+        this._locale.match = {
+            ...this._locale.match,
+            ...{
+                weekdays: buildMatchFn(weekdayValues, "long"),
+                weekday: buildParseFn(weekdayValues, "long"),
+                months: buildMatchFn(monthValues, "long"),
+                month: buildParseFn(monthValues, "long")
+            }
+        };
+    }
+
+    public format(d:Date, f:string):string {
+        return format(d, f, this._config);
+    }
+
+    public parse(dS:string, f:string, bD:Date):Date {
+        return parse(dS, f, bD, this._config);
+    }
+}
