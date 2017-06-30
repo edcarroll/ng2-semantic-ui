b009ac877b270631edde9a1c71414c7380925098
Ed Carroll <ed@edcarroll.co.uk>
2017-06-30 16:14:09 +0100

    feat(localization): Added es & pt locales
    Thanks to @genuinefafa for Spanish translations
    Thanks to @fniwes for Portuguese translations

--- src/behaviors/localization/locales/es.ts
+++ src/behaviors/localization/locales/es.ts
@@ -1,5 +1,10 @@
 import { IPartialLocaleValues } from "../interfaces/values";
 
+/**
+ * locale : Spanish (es)
+ * author : Facundo Donato : https://github.com/genuinefafa
+ */
+
 const es:IPartialLocaleValues = {
     datepicker: {
         months: [
@@ -12,18 +17,35 @@ const es:IPartialLocaleValues = {
             "Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"
         ],
         weekdaysShort: [
-            "Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb"
+            "Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sáb"
         ],
         weekdaysNarrow: [
             "D", "L", "M", "M", "J", "V", "S"
         ],
-        firstDayOfWeek: 1,
         formats: {
             time: "HH:mm",
-            datetime: "DD-MMM-YYYY HH:mm",
-            date: "DD-MMM-YYYY",
-            month: "MMMM YYYY",
+            datetime: "D MMMM de YYYY HH:mm",
+            date: "D de MMMM de YYYY",
+            month: "MMMM de YYYY",
             year: "YYYY"
+        },
+        firstDayOfWeek: 1
+    },
+    search: {
+        placeholder: "Buscar...",
+        noResults: {
+            header: "Sin resultados",
+            message: "La búsqueda no obtuvo resultados."
+        }
+    },
+    select: {
+        noResultsMessage: "Sin resultados",
+        single: {
+            placeholder: "Seleccione uno"
+        },
+        multi: {
+            placeholder: "Seleccione...",
+            maxSelectedMessage: "Puede elegir hasta #{max}."
         }
     }
 };
--- src/behaviors/localization/locales/pt.ts
+++ src/behaviors/localization/locales/pt.ts
@@ -1,7 +1,53 @@
 import { IPartialLocaleValues } from "../interfaces/values";
 
-const pt:IPartialLocaleValues = {
+/**
+ * locale : Portuguese (pt)
+ * author : Fernando Niwes : https://github.com/fniwes
+ */
 
+const pt:IPartialLocaleValues = {
+    datepicker: {
+        months: [
+            "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+        ],
+        monthsShort: [
+            "Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"
+        ],
+        weekdays: [
+            "Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"
+        ],
+        weekdaysShort: [
+            "Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"
+        ],
+        weekdaysNarrow: [
+            "D", "1", "2", "3", "4", "5", "S"
+        ],
+        formats: {
+            time: "HH:mm",
+            datetime: "D MMMM de YYYY HH:mm",
+            date: "D de MMMM de YYYY",
+            month: "MMMM de YYYY",
+            year: "YYYY"
+        },
+        firstDayOfWeek: 1
+    },
+    search: {
+        placeholder: "Pesquisar...",
+        noResults: {
+            header: "Sem resultados",
+            message: "Sua pesquisa não retornou resultados."
+        }
+    },
+    select: {
+        noResultsMessage: "Sem resultados",
+        single: {
+            placeholder: "Escolha uma opção."
+        },
+        multi: {
+            placeholder: "Escolher...",
+            maxSelectedMessage: "Max #{max} opçãoes"
+        }
+    }
 };
 
 export default pt;
