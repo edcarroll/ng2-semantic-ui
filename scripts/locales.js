9f55f616a772adbc713803ddcb810ccaabd797ef
Ed Carroll <ed@edcarroll.co.uk>
2017-07-03 23:52:41 +0100

    fix(localization): Fixed built locale references

--- scripts/locales.js
+++ scripts/locales.js
@@ -5,4 +5,17 @@ const path = require("path");
 const localesSrc = path.resolve("./dist/behaviors/localization/locales");
 const localesDest = path.resolve("./locales");
 
+// Copy locales
 fs.copySync(localesSrc, localesDest);
+fs
+    .readdirSync(localesDest)
+    .filter(f => f.includes(".d.ts"))
+    .forEach(f => {
+        // Rewrite references
+        const rewritten = fs
+            .readFileSync(`${localesDest}/${f}`)
+            .toString()
+            .replace("../interfaces/values", "../dist")
+
+        fs.writeFileSync(`${localesDest}/${f}`, rewritten)
+    });
