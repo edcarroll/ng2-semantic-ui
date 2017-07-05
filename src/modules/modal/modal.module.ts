dfec2808ceaa4026cc4bfbdf0d5c7430f956cfa1
Ed Carroll <ed@edcarroll.co.uk>
2017-07-05 11:33:14 +0100

    fix: Added missing ngModule imports to modal & select

--- src/modules/modal/modal.module.ts
+++ src/modules/modal/modal.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SuiDimmerModule } from "../dimmer";
 import { SuiTransitionModule } from "../transition";
+import { SuiUtilityModule } from "../../misc/util";
 import { SuiModalService } from "./services/modal.service";
 import { SuiModal } from "./components/modal";
 
@@ -9,7 +10,8 @@ import { SuiModal } from "./components/modal";
     imports: [
         CommonModule,
         SuiDimmerModule,
-        SuiTransitionModule
+        SuiTransitionModule,
+        SuiUtilityModule
     ],
     declarations: [
         SuiModal
