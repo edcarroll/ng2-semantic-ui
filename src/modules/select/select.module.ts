e6f2b8bfa17bf7ced8ee095000a9f39c11603acb
Ed Carroll <ed@edcarroll.co.uk>
2017-07-05 11:33:14 +0100

    fix: Added missing ngModule imports to modal & select

--- src/modules/select/select.module.ts
+++ src/modules/select/select.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { SuiDropdownModule } from "../dropdown";
+import { SuiUtilityModule } from "../../misc/util";
+import { SuiLocalizationModule } from "../../behaviors/localization";
 import { SuiSelect, SuiSelectValueAccessor } from "./components/select";
 import { SuiSelectOption } from "./components/select-option";
 import { SuiSelectSearch } from "./directives/select-search";
@@ -12,7 +14,9 @@ import { SuiMultiSelectLabel } from "./components/multi-select-label";
     imports: [
         CommonModule,
         FormsModule,
-        SuiDropdownModule
+        SuiDropdownModule,
+        SuiUtilityModule,
+        SuiLocalizationModule
     ],
     declarations: [
         SuiSelect,
