5c45e95c41776037f367be26bfd80d21975b0ca5
Trevor Daniels <trevor.daniels@gmail.com>
2017-08-08 10:31:48 +1000

    feat(localization): Add Australian locale

---
+++ src/behaviors/localization/locales/en-AU.ts
@@ -0,0 +1,10 @@
+
+import { IPartialLocaleValues } from "../interfaces/values";
+
+const enAU:IPartialLocaleValues = {
+    datepicker: {
+        firstDayOfWeek: 0
+    }
+};
+
+export default enAU;
