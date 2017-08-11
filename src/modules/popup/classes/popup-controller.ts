5f8d6cbeb8bc30f77921a4ffeb6b67f6ad3c51fe
Edward Carroll <edcarroll@users.noreply.github.com>
2017-08-11 18:50:24 +0100

    fix(popup): Fixed conflict with BrowserAnimationsModule (#234)
    * fix(popup): Fixed conflict with BrowserAnimationsModule
    
    Closes #204
    
    * style(popup): Fixed tslint error

--- src/modules/popup/classes/popup-controller.ts
+++ src/modules/popup/classes/popup-controller.ts
@@ -65,8 +65,7 @@ export abstract class SuiPopupController implements IPopup, OnDestroy {
             });
         }
 
-        // Detach & reattach the generated component to the current application.
-        this._componentFactory.detachFromApplication(this._componentRef);
+        // Attach the generated component to the current application.
         this._componentFactory.attachToApplication(this._componentRef);
 
         // Move the generated element to the body to avoid any positioning issues.
@@ -151,7 +150,6 @@ export abstract class SuiPopupController implements IPopup, OnDestroy {
         }
     }
 
-    // @HostListener("document:click", ["$event"])
     public onDocumentClick(e:MouseEvent):void {
         // If the popup trigger is outside click,
         if (this._componentRef && this.popup.config.trigger === PopupTrigger.OutsideClick) {
@@ -179,6 +177,7 @@ export abstract class SuiPopupController implements IPopup, OnDestroy {
 
     public ngOnDestroy():void {
         clearTimeout(this._openingTimeout);
+        this._componentFactory.detachFromApplication(this._componentRef);
         this._componentRef.destroy();
     }
 }
