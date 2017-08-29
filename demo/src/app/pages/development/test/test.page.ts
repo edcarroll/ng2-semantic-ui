6a22796a0646f8962a931f25a41863c4a730de3a
vgazdeliani <vgazdeliani@bog.ge>
2017-08-29 17:32:14 +0400

    tslint

--- demo/src/app/pages/development/test/test.page.ts
+++ demo/src/app/pages/development/test/test.page.ts
@@ -6,18 +6,20 @@ import { FormControl, Validators } from "@angular/forms";
     templateUrl: "./test.page.html"
 })
 export class TestPage implements OnInit {
-    private _interval: any;
-    public ngOnInit(): void {
+    private _interval:any;
+    public progress:number;
+
+    constructor() {
+        this.progress = 0;
+    }
+
+    public ngOnInit():void {
         this._interval = setInterval(() => {
             this.progress = this.progress + 1;
             if (this.progress === 100) {
                 clearInterval(this._interval);
             }
         },
-            100);
-    }
-    public progress: number;
-    constructor() {
-        this.progress = 0;
+        100);
     }
 }
