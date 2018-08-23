944abf3620fe3db35e42f197eb40e334cc6f5d07
Edward Carroll <edcarroll@users.noreply.github.com>
2018-08-24 03:09:28 +0800

    fix(popup): Don't use the site to merge

--- src/modules/popup/directives/popup.directive.ts
+++ src/modules/popup/directives/popup.directive.ts
@@ -1,11 +1,10 @@
 import { Directive, Input, ElementRef, TemplateRef, Renderer2 } from "@angular/core";
 import { ITemplateRefContext, Util, PositioningPlacement, SuiComponentFactory } from "../../../misc/util/internal";
 import { SuiPopup } from "../components/popup";
-import { PopupConfig, PopupTrigger } from "../classes/popup-config";
+import { PopupConfig, PopupTrigger, PopupSize, PopupWidth } from "../classes/popup-config";
 import { SuiPopupConfig } from "../services/popup.service";
 import { SuiPopupController } from "../classes/popup-controller";
 import { SuiPopupTemplateController, ITemplatePopupContext, ITemplatePopupConfig } from "../classes/popup-template-controller";
-import { PopupSize, PopupWidth } from "../classes/popup-config";
 
 const templateRef = TemplateRef;
 
